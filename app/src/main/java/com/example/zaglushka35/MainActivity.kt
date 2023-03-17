dedeb05f6f999d6241698c2166b47ebc7fd1d443
DanielPHP01 <danielmokushov01@gmail.com>
2023-03-17 20:18:00 +0600

    update try again

--- app/src/main/java/com/example/zaglushka35/MainActivity.kt
+++ app/src/main/java/com/example/zaglushka35/MainActivity.kt
@@ -1,25 +1,14 @@
 package com.example.zaglushka35
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.zaglushka35.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-        wordToGuess = generateWord()
-    }
-
     private lateinit var wordToGuess: String
     private var errors: Int = 0
     private val hangmanImages = arrayOf(
@@ -29,16 +18,20 @@ class MainActivity : AppCompatActivity() {
         R.drawable.wisielec11
     )
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        wordToGuess = generateWord()
+    }
+
     fun generateWord(): String {
         val words: Array<String> = resources.getStringArray(R.array.words_categories)
         val random = Random.nextInt(words.size)
         var word = words[random]
         var text = ""
-        for(letter in word){
-            text += if (letter.isLetter()){
-                "_"
-            } else
-                " "
+        for (letter in word) {
+            text += if (letter.isLetter()) "_" else " "
         }
         binding.textViewWord.text = text
         return words[random]
@@ -48,47 +41,89 @@ class MainActivity : AppCompatActivity() {
         if (wordToGuess.contains(letterGuessed, ignoreCase = true)) {
             var newText = ""
             var actualText = binding.textViewWord.text.toString()
-            for(i in wordToGuess.indices){
-                newText += if (!wordToGuess[i].isLetter()){
-                    " "
-                } else if (wordToGuess[i].toLowerCase() != letterGuessed.toLowerCase()) {
-                    actualText[i]
-                }
-                else {
-                    letterGuessed
-                }
+            for (i in wordToGuess.indices) {
+                newText += if (!wordToGuess[i].isLetter()) " " else if (wordToGuess[i].toLowerCase() != letterGuessed.toLowerCase()) actualText[i] else letterGuessed
             }
             binding.textViewWord.text = newText
-            if(!binding.textViewWord.text.toString().contains("_")){
-                val buttons = arrayOf(binding.buttonA, binding.buttonB, binding.buttonC,
-                    binding.buttonD, binding.buttonE, binding.buttonF, binding.buttonG, binding.buttonH,
-                    binding.buttonI, binding.buttonJ, binding.buttonK, binding.buttonL, binding.buttonM,
-                    binding.buttonN, binding.buttonO, binding.buttonP, binding.buttonQ, binding.buttonR,
-                    binding.buttonS, binding.buttonT, binding.buttonU, binding.buttonV, binding.buttonW,
-                    binding.buttonX, binding.buttonY, binding.buttonZ)
-                for(button in buttons)
-                    button.isEnabled = false
+            if (!binding.textViewWord.text.toString().contains("_")) {
+                onTryAgain()
+                val buttons = arrayOf(
+                    binding.buttonA,
+                    binding.buttonB,
+                    binding.buttonC,
+                    binding.buttonD,
+                    binding.buttonE,
+                    binding.buttonF,
+                    binding.buttonG,
+                    binding.buttonH,
+                    binding.buttonI,
+                    binding.buttonJ,
+                    binding.buttonK,
+                    binding.buttonL,
+                    binding.buttonM,
+                    binding.buttonN,
+                    binding.buttonO,
+                    binding.buttonP,
+                    binding.buttonQ,
+                    binding.buttonR,
+                    binding.buttonS,
+                    binding.buttonT,
+                    binding.buttonU,
+                    binding.buttonV,
+                    binding.buttonW,
+                    binding.buttonX,
+                    binding.buttonY,
+                    binding.buttonZ
+                )
+                buttons.forEach { button -> button.isEnabled = false }
             }
         } else {
             errors++;
             binding.imageView.setImageResource(hangmanImages[errors])
-            if(errors+1 >= hangmanImages.size){
+            if (errors + 1 >= hangmanImages.size) {
+                onTryAgain()
                 binding.textViewWord.text = wordToGuess
-                val buttons = arrayOf(binding.buttonA, binding.buttonB, binding.buttonC,
-                    binding.buttonD, binding.buttonE, binding.buttonF, binding.buttonG, binding.buttonH,
-                    binding.buttonI, binding.buttonJ, binding.buttonK, binding.buttonL, binding.buttonM,
-                    binding.buttonN, binding.buttonO, binding.buttonP, binding.buttonQ, binding.buttonR,
-                    binding.buttonS, binding.buttonT, binding.buttonU, binding.buttonV, binding.buttonW,
-                    binding.buttonX, binding.buttonY, binding.buttonZ)
-                for(button in buttons)
-                    button.isEnabled = false
+                val buttons = arrayOf(
+                    binding.buttonA,
+                    binding.buttonB,
+                    binding.buttonC,
+                    binding.buttonD,
+                    binding.buttonE,
+                    binding.buttonF,
+                    binding.buttonG,
+                    binding.buttonH,
+                    binding.buttonI,
+                    binding.buttonJ,
+                    binding.buttonK,
+                    binding.buttonL,
+                    binding.buttonM,
+                    binding.buttonN,
+                    binding.buttonO,
+                    binding.buttonP,
+                    binding.buttonQ,
+                    binding.buttonR,
+                    binding.buttonS,
+                    binding.buttonT,
+                    binding.buttonU,
+                    binding.buttonV,
+                    binding.buttonW,
+                    binding.buttonX,
+                    binding.buttonY,
+                    binding.buttonZ
+                )
+                buttons.forEach { button -> button.isEnabled = false }
             }
         }
     }
-
     fun onClickLetter(view: View) {
         (view as Button).isEnabled = false
         val letter = view.text.toString()[0]
         checkLetter(letter)
+    }
+    fun onTryAgain() {
+        binding.btnTryAgain.visibility = View.VISIBLE
+        binding.btnTryAgain.setOnClickListener{
+            recreate()
+        }
     }
 }
